be697d015f6877785bc3db0e9aac7f96e4d32560
Gautam104 <148959197+Gautam104@users.noreply.github.com>
2026-04-23 21:44:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,12 +57,7 @@ if cost_file and panding_file and lab_file:
         "Cts.",
         "No of Days",
         "Lab",
-        "Quality",
-        "UPDATED PRICE",
-        "DIFFERENCE",
-        "Cost Amt",
-        "Sale Amt",
-        "Difference"
+        "Quality"
     ]]
 
     # ================= OUTPUT =================
